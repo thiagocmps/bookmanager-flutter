5aa5f2b9e6e57662c95f11d536071df8ad2d4887
Bernardo Moreira <bernardocosta972@gmail.com>
2024-12-27 15:04:46 +0000

    Test of register post(incomplete)
    Incomplete post test to decide whether to place the API in the cloud or localhost

--- pi_flutter_application/lib/screens/register_screen.dart
+++ pi_flutter_application/lib/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -64,12 +65,26 @@ class _RegisterPageState extends State<RegisterPage> {
             ),
             SizedBox(height: 16),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pop(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => RegisterPage(),
-                    ));
+              onPressed: () async {
+                try {
+                  final response = await http.post(
+                    Uri.parse(''),
+                    headers: {'Content-Type': 'application/json'},
+                    body: jsonEncode({
+                      'email': email,
+                      'password': password,
+                      'nome': username,
+                    }),
+                  );
+                if (response.statusCode == 201) {
+                  Navigator.push(
+                    context, 
+                  MaterialPageRoute(builder: (context) => RegisterPage()),
+                  );
+                }
+                } catch (error) {
+                  print('Error: $error');
+                }
               },
               child: Text('Criar conta'),
             ),
